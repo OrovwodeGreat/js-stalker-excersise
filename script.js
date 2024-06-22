7fe65b85334038c39d5155ca305cdef12c861196
Orovwode Great <orovwodegreat@gmail.com>
2024-06-22 16:52:13 +0100

    another commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?"); 
+var lastName = prompt("What is your last name?"); 
+var ageValue = prompt("How old are you?");
+console.log("Your full name is " + lastName + " " + firstName);
+console.log("Your are " + ageValue + " years old");
